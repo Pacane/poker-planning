48757a328761975f2d216a9e2c45e9e610592d08
Joel Trottier-Hebert <iamquebecois@gmail.com>
2015-01-22 16:53:00 -0500

    Used the static routes in the components

--- PokerPlanningClient/lib/routes.dart
+++ PokerPlanningClient/lib/routes.dart
@@ -1,9 +1,9 @@
 library routes;
 
 class Routes {
-  static String get GAME => 'game';
-  static String get LOGIN => 'login';
-  static String get ROOT => 'root';
+  static const String GAME = 'game';
+  static const String LOGIN = 'login';
+  static const String ROOT = 'root';
 
   static String toPath(String routeName) => '/'+ routeName;
 }
